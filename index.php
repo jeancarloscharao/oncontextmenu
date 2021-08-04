fe843a7da6eddaf3ac6831d13ba3ae50162965bc
Jean Carlos Charão Sabino <jeancarloscharao@gmail.com>
2021-08-03 21:44:40 -0300

    Atualizado para proteger 100% div

--- index.php
+++ index.php
@@ -11,11 +11,12 @@
     <title>Hello, world!</title>
   </head>
   <body>
+    <div class="container" oncontextmenu="return false" style="height: 100vh">
     <h1>Hello, world!</h1>
 
 
 
-<div oncontextmenu="return false">
+<div>
 		<form>
 		  <div class="mb-3">
 		    <label for="exampleInputEmail1" class="form-label">Email address</label>
@@ -46,6 +47,8 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
+
+    </div>
   </body>
 </html>
 
